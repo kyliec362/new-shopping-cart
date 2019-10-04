8530fea2b742bc7f7eec0e9a8d6b880018bb7cda
Kylie Chesner <kyliec362@gmail.com>
2019-10-04 09:55:41 -0500

    styled static page with product images & data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React, { useEffect, useState } from 'react';
+import { Grid } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import { Checkbox } from '@material-ui/core';
+
+const availableSizes = ['XS', 'S', 'M', 'ML', 'L', 'XL', 'XXL'];
+
+class SizeFilter extends React.Component {
+
+    componentDidMount() {
+      this.selectedCheckboxes = new Set();
+    };
+
+    makeCheckbox = label => (
+        <Checkbox
+        label={label}
+        handleCheckboxChange={this.toggleCheckbox}
+        key={label}
+      />
+      );
+
+    makeCheckboxes = () => Array.from(this.selectedCheckboxes).map(this.createCheckbox);
+
+  
+    toggleCheckbox = label => {
+      if (this.selectedCheckboxes.has(label)) {
+        this.selectedCheckboxes.delete(label);
+      } else {
+        this.selectedCheckboxes.add(label);
+      }
+    }
+
+    render() {
+      return(
+        <div>
+          {this.createCheckboxes()}
+        </div>
+        )
+      };
+}
+class Product extends React.Component {
+  render() {
+    const product = this.props.product;
+    return (
+      <Card style={{textAlign: 'center', width: '25%', margin: '0.5%'}}>
+      <img src={`./data/products/${product.sku}_2.jpg`} alt=''/>
+      <div>{product.title}</div>
+      <div>${product.price}</div>
+    </Card>
+    )
+  }
+
+}
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('./data/products.json');
+      const response = await fetch('./data/productTitles.json');
       const json = await response.json();
       setData(json);
     };
@@ -13,9 +65,9 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+    <Grid container spacing={10} justify="center">
+      {products.map(product => <Product product={product}>{product.title}</Product>)}
+    </Grid>
   );
 };
 
