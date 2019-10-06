faf89f6038d083a1c1a91bd308a25913893f4d6e
Kylie Chesner <kyliec362@gmail.com>
2019-10-06 01:58:06 -0500

    only instock sizes shown

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Modal from '@material-ui/core/Modal';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
@@ -24,6 +21,8 @@ const availableSizes = ['S', 'M', 'L', 'XL'];
 function roundToTwo(num) {    
   return +(Math.round(num + "e+2")  + "e-2");
 }
+
+
 
 class CartListItem extends React.Component {
 
@@ -101,36 +100,43 @@ class CartLogo extends React.Component {
 
 class SizeFilter extends React.Component {
 
-    componentDidMount() {
-      this.selectedCheckboxes = new Set();
-    };
+  toggleCheckbox = (label, selectedCheckboxes, setSelectedCheckboxes, updateFlag, setUpdateFlag) => {
+    if (selectedCheckboxes.has(label)) 
+      selectedCheckboxes.delete(label);
+    else 
+      selectedCheckboxes.add(label);
+    setSelectedCheckboxes(selectedCheckboxes);
+    console.log(109, selectedCheckboxes);
+    setUpdateFlag(!updateFlag);
 
-    makeCheckbox = label => (
+  }
+
+   makeCheckbox = (label, selectedCheckboxes, setSelectedCheckboxes,updateFlag, setUpdateFlag) => (
       <FormControlLabel
       control={
-        <Checkbox onChange={this.toggleCheckbox} value={label} />
+        <Checkbox onChange={() => this.toggleCheckbox(label, selectedCheckboxes, setSelectedCheckboxes, updateFlag, setUpdateFlag)} value={label} />
       }
       label={label}
     />
     );
 
-    makeCheckboxes = () => availableSizes.map(this.makeCheckbox);
+  makeCheckboxes = (selectedCheckboxes, setSelectedCheckboxes, updateFlag, setUpdateFlag, ) => availableSizes.map(label => this.makeCheckbox(label, selectedCheckboxes, setSelectedCheckboxes, updateFlag, setUpdateFlag));
 
-    toggleCheckbox = label => {
-      if (this.selectedCheckboxes.has(label)) {
-        this.selectedCheckboxes.delete(label);
-      } else {
-        this.selectedCheckboxes.add(label);
-      }
-    }
+
 
     render() {
+
+      const selectedCheckboxes = this.props.selectedCheckboxes;
+      const setSelectedCheckboxes = this.props.setSelectedCheckboxes;
+      const updateFlag = this.props.updateFlag;
+      const setUpdateFlag = this.props.setUpdateFlag;
+
       return(
         <div style={{marginRight: '3%', marginLeft: '5%', float: 'left'}}>
         <FormControl>
           <FormLabel >Sizes</FormLabel>
           <FormGroup>
-            {this.makeCheckboxes()}
+            {this.makeCheckboxes(selectedCheckboxes, setSelectedCheckboxes, updateFlag, setUpdateFlag)}
           </FormGroup>
         </FormControl>
         </div>
@@ -138,10 +144,51 @@ class SizeFilter extends React.Component {
       };
 }
 
+class ProductGrid extends React.Component {
+
+  sizeNotSoldOut = (product, size, inventory) => {
+    if (inventory[product.sku] != null) {
+      return inventory[product.sku][size] > 0;
+    }
+    return false;
+  };
+
+  someSizeNotSoldOut = (product, selectedCheckboxes, inventory) => {
+    return Array.from(selectedCheckboxes).some(size => this.sizeNotSoldOut(product, size, inventory));
+  };
+
+  filterProducts = (products, selectedCheckboxes, inventory) => {
+    if (Array.from(selectedCheckboxes).length == 0) {//if no size filter placed, show all products
+      return products
+  }
+    else
+      return products.filter(product => this.someSizeNotSoldOut(product, selectedCheckboxes, inventory));
+  };
+
+  render() {
+    const products = this.props.products;
+    const inventory = this.props.inventory;
+    const cartItems = this.props.cartItems; 
+    const setCartItems = this.props.setCartItems; 
+    const cartQuantities = this.props.cartQuantities;
+    const setCartQuantities = this.props.setCartQuantities; 
+    const selectedCheckboxes = this.props.selectedCheckboxes;
+    const setSelectedCheckboxes = this.props.setSelectedCheckboxes;
+
+    var availableProducts = this.filterProducts(products, selectedCheckboxes, inventory);
+    return (
+      <Grid style={{width: '85%', marginTop: '0%'}} container spacing={10} justify="center"  >
+        {availableProducts.map(product => <Product stock = {inventory[product.sku]} selectedCheckboxes = {selectedCheckboxes} cartQuantities = {cartQuantities} setCartQuantities = {setCartQuantities} cartItems = {cartItems} setCartItems = {setCartItems} product={product}>{product.title}</Product>)}
+      </Grid>
+    );
+  };
+};
+
+
+
 class Product extends React.Component {
 
   addToCart = (setCartItems, cartItems, product, cartQuantities, setCartQuantities) => {
-    console.log(cartQuantities)
     if (cartQuantities.has(product.title)) { //already in cart, just adding to quantity 
       cartQuantities.set(product.title, cartQuantities.get(product.title) + 1);
       setCartQuantities(cartQuantities); 
@@ -181,16 +228,28 @@ const App = () => {
   const [isCartOpen, setIsCartOpen] = useState('cartOpen');
   const [cartItems, setCartItems] = useState([]); 
   const [cartQuantities, setCartQuantities] = useState(new Map()); 
+  const [inventory, setInventory] = useState({});
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState(new Set());
+  const [updateFlag, setUpdateFlag] = useState(false);
   const products = Object.values(data);
 
   useEffect(() => {
     setIsCartOpen(false);
     const fetchProducts = async () => {
-      const response = await fetch('./data/productTitles.json');
+      const response = await fetch('./data/products.json');
       const json = await response.json();
       setData(json);
     };
     fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    const fetchInventory = async () => {
+      const response = await fetch('./data/inventory.json');
+      const json = await response.json();
+      setInventory(json);
+    };
+    fetchInventory();
   }, []);
 
   const handleCartOpen = () => {
@@ -207,11 +266,9 @@ const App = () => {
     <Modal open={isCartOpen} onClose={handleCartClose}>
       {<Cart cartItems = {cartItems} setCartItems = {setCartItems} cartQuantities = {cartQuantities} setCartQuantities = {setCartQuantities}></Cart>}
     </Modal>
-    <Grid style={{marginTop: '10%'}} container>
-      <SizeFilter/>
-      <Grid style={{width: '85%', marginTop: '0%'}} container spacing={10} justify="center"  >
-        {products.map(product => <Product cartQuantities = {cartQuantities} setCartQuantities = {setCartQuantities} cartItems = {cartItems} setCartItems = {setCartItems} product={product}>{product.title}</Product>)}
-      </Grid>
+    <Grid style={{marginTop: '10%'}} container updateFlag = {updateFlag} setUpdateFlag = {setUpdateFlag}> 
+      <SizeFilter updateFlag = {updateFlag} setUpdateFlag = {setUpdateFlag} selectedCheckboxes = {selectedCheckboxes} setSelectedCheckboxes = {setSelectedCheckboxes}/>
+      <ProductGrid selectedCheckboxes = {selectedCheckboxes} setSelectedCheckboxes = {setSelectedCheckboxes} products = {products} inventory = {inventory} cartItems = {cartItems} setCartItems = {setCartItems} cartQuantities = {cartQuantities} setCartQuantities = {setCartQuantities}></ProductGrid>
     </Grid>
     </div>
   );
